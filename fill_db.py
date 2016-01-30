1b425abae6a1747192d5f3b4cca181dc378e08c2
Ricardo Alanis <ricardo.alanis@gmail.com>
2016-01-30 01:11:38 -0600

    changes to fill_db

--- fill_db.py
+++ fill_db.py
@@ -1,10 +1,10 @@
-from pymongo import MongoClient
+from pymongo import MongoClient, GEOSPHERE
 import uuid
 import hashlib
 
 client = MongoClient()
 db = client.test
-db.test_fetch.ensure_index("location", "2dsphere")
+db.test_fetch.ensure_index([("location", GEOSPHERE)])
 
 #Obtenido de: http://pythoncentral.io/hashing-strings-with-python/
 def hash_password(password):
@@ -15,18 +15,18 @@ def hash_password(password):
 def return_geopoint(longitude, latitude):
     geopoint = {
         "type" : "Point",
-        "coordinates" : [ longitude, latitude ]
+        "coordinates" : [ float(longitude), float(latitude) ]
     }
     return geopoint
 
 def has_password(password):
     has = 0
-    if password not "":
+    if password is not "":
         has= 1
     return has
 
 def build_object_dictionary(name, description, link, crude_password, longitude, latitude):
-    has_pwd = has_password(password)
+    has_pwd = has_password(crude_password)
     object_dictionary = {
     "name": name,
     "description": description,
@@ -37,7 +37,6 @@ def build_object_dictionary(name, description, link, crude_password, longitude, 
     }
     return object_dictionary
 
-#Archivos para probar cerca de "-100.2952296","25.648925"
 def create_dummy_objects():
     db.test_fetch.insert_one(build_object_dictionary("Pokemon Red", "best game evuuuur", "http://pokemon-stats.com/descargas/pokemon_rojo.zip", "rojo", "-100.2952296","25.648925"))
     db.test_fetch.insert_one(build_object_dictionary("Pokemon Azul", "best game evuuuur x 2", "http://pokemon-stats.com/descargas/pokemon_azul.zip", "azul","-100.2937973","25.6483229"))
@@ -45,5 +44,6 @@ def create_dummy_objects():
     db.test_fetch.insert_one(build_object_dictionary("Pokemon Dorado", "best game evuuuur x 4", "http://pokemon-stats.com/descargas/pokemon_dorado.zip", "Dorado","-100.2921451","25.6506393"))
     db.test_fetch.insert_one(build_object_dictionary("Pokemon Plata", "best game evuuuur x 5", "http://pokemon-stats.com/descargas/pokemon_plata.zip", "Plata","-100.2887118","25.6315655"))
     return "Success"
+
 
 create_dummy_objects()
